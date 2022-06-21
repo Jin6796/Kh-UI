2d514daefbbf45e905e6bb84f266cfc1714b6d79
Jin <cc6796@naver.com>
2022-06-21 09:49:02 +0900

    20220621

---
+++ day1/main.js
@@ -0,0 +1,13 @@
+function add(a, b){
+    return a+b;}
+
+console.log(add(1,2)); 
+console.log(add(2,3));
+
+let colors = require('colors');
+console.log(colors.rainbow('Hello!!!'));
+console.log(colors.red('Hello!!!'));
+console.log('test');
+console.log('dd');
+
+
