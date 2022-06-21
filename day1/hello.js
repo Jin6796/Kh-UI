c9ca86e2da2e41094038461c3ba191568eeaac1f
Jin <cc6796@naver.com>
2022-06-21 11:00:23 +0900

    0621

--- day1/hello.js
+++ day1/hello.js
@@ -2,4 +2,5 @@ let colors = require('colors');
 
 console.log(colors.rainbow('Hello NodeJS!!!'));
 console.log(colors.red('Hello NodeJS!!!'));
-
+console.log(colors.green('fu'));
+console.log(colors.green('uu'));
