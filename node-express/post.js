d6ef2d65a0f072fcd5e7c440305d2641c32a436b
Jin <cc6796@naver.com>
2022-07-12 09:02:38 +0900

    0712

--- node-express/post.js
+++ node-express/post.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
-// nodejs로부터 요청객체와 응답객체는 주입을 받게 됨.
+app.use(express.json());
 app.post("/", (req, res) =>{
   console.log(req.body);
 });
 
-app.listen(3003);
+app.listen(3000);
 
 /* 
   http://localhost:3003/kh/tomato  -> id값은 tomato
--- render.js
+++ render.js
@@ -1,6 +1,8 @@
 const express = require("express"); 
 
 const app = express();
+app.use("/public", express.static(__dirname+"/public"));
+
 app.set("view engine", "ejs");
 
 app.get("/hello/:nameParam", (req,res) => {
