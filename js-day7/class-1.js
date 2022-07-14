cb6e6755ebf0c9ed335015eb3ed098c54b45adde
Jin <cc6796@naver.com>
2022-07-14 10:05:13 +0900

    0714(추가)

--- js-day7/class-1.js
+++ js-day7/class-1.js
@@ -1,10 +1,13 @@
 // 클래스 - ES6에서 지원하게 됨
-
+// 리액트 - 양이 늘어나더라도 복잡도는 증가하지 않는...
 class Car {
+// 생성자 함수에서 초기화 가능하다
+// 공통분모: 전변 초기화를 담당한다 - 생성자 함수가
   constructor(name, emoji){
     this.name = name;
     this.emoji = emoji;
   }
+  // 사용자 정의 함수를 가질 수 있다. -> 제공되는 함수가 있다. - window, document.write()
   display = () => {
     console.log(`${this.name}, ${this.emoji}`);
   }
